a995c8116ba3473dbf639d9308ed740a0a31c40e
Camila Arias <ariasruicamilaa>
2024-02-22 09:41:44 -0500

    token time

--- de_castilla_back/settings.py
+++ de_castilla_back/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 from pathlib import Path
+from datetime import timedelta
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -90,6 +92,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'USER_ID_FIELD': 'no_documento_usuario',
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=2),
     # otras configuraciones de SIMPLE_JWT
 }
 
