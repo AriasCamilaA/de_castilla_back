38b6b50e8114a716657e1f71375903453dbd96b8
sebastian <sebastianjuncor@gmail.com>
2024-03-11 00:17:07 -0500

    productos generate_pdf completo

--- apps/producto/api/router.py
+++ apps/producto/api/router.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('productos/', ProductoViewSet.as_view({'get': 'list', 'post': 'create'})),
     path('productos/<int:pk>/', ProductoViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
     path('productos/generate-pdf/', generate_pdf, name='generate_pdf'),
+    path('productos/generate-pdf/<filtro>/', generate_pdf, name='generate_pdf'),
 ]
