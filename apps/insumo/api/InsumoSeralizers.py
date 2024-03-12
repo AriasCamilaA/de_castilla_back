b43bac6add3980e1ad0315dadb98fa55452ac223
Camila Arias <ariasruicamilaa>
2024-03-12 01:06:14 -0500

    Borrar Estado Insumo

--- apps/insumo/api/InsumoSeralizers.py
+++ apps/insumo/api/InsumoSeralizers.py
@@ -1,13 +1,11 @@
 from rest_framework import serializers
 from ..models import *
 from rest_framework import serializers
-from apps.estado_insumo.api.EstadoInsumoSeralizers import EstadoInsumoSeralizers
 
 
 
 
 class InsumoSeralizers(serializers.ModelSerializer):
-    estado_insumo = EstadoInsumoSeralizers(source='id_estado_insumo_fk', read_only=True)
     class Meta:
         model = Insumo
         fields = '__all__'  # Incluir todos los campos del modelo
