6a557ce5e8f7f682741c5d62c8af90aedc815e6e
Camila Arias <ariasruicamilaa>
2024-03-14 12:32:20 -0500

    Eliminar app Correo

--- apps/correo/models.py
+++
@@ -1,8 +0,0 @@
-# correo_app/models.py
-
-from django.db import models
-
-class Correo(models.Model):
-    destinatario = models.EmailField()
-    asunto = models.CharField(max_length=255)
-    html = models.TextField()  # Campo para el HTML del correo
