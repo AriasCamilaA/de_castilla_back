1109af561f33a5c264648027501b0329c6314dee
Camila Arias <ariasruicamilaa>
2024-03-16 10:43:45 -0500

    add host to render

--- de_castilla_back/settings.py
+++ de_castilla_back/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-eq6p5-!fynf4+p#q5z$g(!jibp5ng_ky)v-hg67s6oacppm_w=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['de-castilla-back.onrender.com']
 
 
 # Application definition
